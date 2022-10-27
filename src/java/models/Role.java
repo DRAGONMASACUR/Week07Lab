624d2bab42d578750419250ff0adbc1e9aeb54ae
DRAGONMASACUR <1dragon_slay@live.ca>
2022-10-27 09:40:44 -0600

    Made models

--- src/java/models/Role.java
+++ src/java/models/Role.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
+
+import java.io.Serializable;
 
 /**
  *
  * @author 1drag
  */
-public class Role {
+public class Role implements Serializable {
+
+    int roleID;
+    String roleName;
+
+    public Role() {
+        roleID = 2;
+    }
+
+    public Role(int roleID, String roleName) {
+        this.roleID = roleID;
+        this.roleName = roleName;
+    }
+
+    public void setRoleID(int roleID) {
+        this.roleID = roleID;
+    }
+
+    public void setroleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public int getRoleID() {
+        return roleID;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
     
 }
